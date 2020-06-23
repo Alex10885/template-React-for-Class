3f80821013d2f56da4f2f589da328f80d9106036
Alex10885 <aledor10885@gmail.com>
2020-06-23 16:32:46 +0300

    шаблон React приложения с классами

---
+++ src/footer/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Footer.css';
+
+    class App extends React.Component {
+    render(){
+        return (
+            <footer className="section footer-classic content-dark bg-image" style = {{background: "#2d3246", color: "#fff"}}>
+                © Copyright 
+            </footer>
+
+        )
+    }
+}
+
+export default App;
